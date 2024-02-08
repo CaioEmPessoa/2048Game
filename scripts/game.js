1e76d65057ec0af9aec2d56dd94617dd59d369f8
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-02-07 21:39:30 -0300

    Organized game code into a class

--- scripts/game.js
+++ scripts/game.js
@@ -1,16 +1,32 @@
-let gameTable = {
-    1: [0, 0, 0, 0],
-    2: [0, 0, 0, 0],
-    3: [0, 0, 0, 0],
-    4: [0, 0, 0, 0]
+class StartGame {
+    
+    startValues = () => {
+        this.gameTable = {
+            1: [0, 0, 0, 0],
+            2: [0, 0, 0, 0],
+            3: [0, 0, 0, 0],
+            4: [0, 0, 0, 0]
+        };
+
+        this.numbOptions = [2, 2, 2, 4];
+    }
+    
+    addNumb = () => {
+        const rowNumb = Math.floor(Math.random() * 4)+1;
+        const colNumb = Math.floor(Math.random() * 4);
+        const numb = Math.floor(Math.random() * 4);
+
+        let addPlace = this.gameTable[rowNumb][colNumb];
+
+        if (addPlace==0) {
+            this.gameTable[rowNumb][colNumb] = this.numbOptions[numb];
+        } else {
+            this.addNumb();
+        }
+    }
 }
 
-const numbOptions = [2, 2, 2, 4]
-
-let rowNumb = Math.floor(Math.random() * 4)+1
-let colNumb = Math.floor(Math.random() * 4)
-let numb = Math.floor(Math.random() * 4)
-console.log(rowNumb)
-gameTable[rowNumb][colNumb] = numbOptions[numb]
-
-console.log(gameTable)
+startGame = new StartGame();
+startGame.startValues();
+startGame.addNumb();
+console.log(startGame.gameTable);
