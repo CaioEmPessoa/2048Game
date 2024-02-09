6928cc91c483876f81a4d76cb6bd66878717f8c5
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-02-09 17:19:01 -0300

    changes button colors dynamically

--- scripts/gamePage.js
+++ scripts/gamePage.js
@@ -8,19 +8,40 @@ const generateBoard = () => {
     for(let x=0; x < 4; x++){
         document.getElementById("gameBoard").innerHTML = document.getElementById("gameBoard").innerHTML +`<div id=\"row${x}\"></div>`
         for(let y =0; y < 4; y++){
-            document.getElementById(`row${x}`).innerHTML += `<div class=\"square\" id=\"square${y}${x}\"></div>`
+            document.getElementById(`row${x}`).innerHTML += `<div id=\"square${y}${x}\"></div>`
         }
     }
+}
+
+const tierDict = {
+    2:"one", 4:"two",
+    8:"three", 16:"four",
+    32:"five", 64:"six",
+    128:"seven", 256:"eight",
+    512:"nine", 1024:"ten",
+    2048:"eleven"
 }
 
 const updateBoard = () => {
     console.log(gameLogic.gameTable)
     for(let x = 0; x < 4; x++) {
         for(let y = 0; y < 4; y++){
-            if (gameLogic.gameTable[x][y] != 0){
-                document.getElementById(`square${x}${y}`).innerHTML = gameLogic.gameTable[x][y]
+
+            tileNumb = gameLogic.gameTable[x][y]
+            
+            if (tileNumb != 0){
+                document.getElementById(`square${x}${y}`).innerHTML = tileNumb;
+                
+                document.getElementById(`square${x}${y}`).classList = ""; // clear class
+                
+                document.getElementById(`square${x}${y}`).classList.add("square");
+                document.getElementById(`square${x}${y}`).classList.add('tier-' + tierDict[tileNumb]);
+                
             } else {
-                document.getElementById(`square${x}${y}`).innerHTML = ""
+                document.getElementById(`square${x}${y}`).innerHTML = "";
+                document.getElementById(`square${x}${y}`).classList = ""; // clear class
+
+                document.getElementById(`square${x}${y}`).classList.add("tier-zero", "square");
             }
         }
     }
